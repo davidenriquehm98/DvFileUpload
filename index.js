1587b5eb136c16f9389cc77062b223a13de88215
dehernandezm <davikik2chi5no@gmai.com>
2020-08-14 16:58:00 -0600

    add extenciones y default a prototype en install

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import component from './DvFileUpload.vue'
 import component2 from './DvFileUploadBtn.vue'
+import { Extenciones } from './utils/extenciones.js'
+import { defaultImages } from './utils/defaultImages.js'
 
 export const DvFileUpload = {
   install (Vue) {
+    Vue.prototype.$dvExtenciones = Extenciones
+    Vue.prototype.$dvDefaultImages = defaultImages
     Vue.component('dv-file-upload', component)
   }
 }
 
 export const DvFileUploadBtn = {
   install (Vue) {
+    Vue.prototype.$dvExtenciones = Extenciones
+    Vue.prototype.$dvDefaultImages = defaultImages
     Vue.component('dv-file-upload-btn', component2)
   }
 }
